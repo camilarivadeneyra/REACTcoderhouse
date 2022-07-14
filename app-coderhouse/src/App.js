cc6065e3be3a8e80aae7aaaa62efefb079ec84f3
camilarivadeneyra <c.crivadeneyra@gmail.com>
2022-07-14 11:34:09 -0300

    CLI

--- app-coderhouse/src/App.js
+++ app-coderhouse/src/App.js
@@ -5,9 +5,8 @@ import Navbar from './components/Navbar/Navbar';
 function App () {
   return (
     <div className = "App">
-      <header className = "Aapp-header">
+      <header className = "App-header">
         <Navbar/>
-        <img src = {logo} className = "App-logo" />
         <h6>Cosmetica Natural.</h6>
       </header>
     </div>
