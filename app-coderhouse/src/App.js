7705efa9ccf444932e9d90cce6803cbcfef37193
camilarivadeneyra <c.crivadeneyra@gmail.com>
2022-07-13 21:32:55 -0300

    CLI

--- app-coderhouse/src/App.js
+++ app-coderhouse/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CODERHOUSE
+        <p 
+          className="Parrafo"
+          // style={{ color: 'red', fontSize: 64}}
+        >
+          Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
     </div>
---
+++ app-coderhouse/src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className='Navbar'>
+            <div>
+                T E R R A
+            </div>
+            <div>
+                <ul><li>PRODUCTOS</li></ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
+
+
+
